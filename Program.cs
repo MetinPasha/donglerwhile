e20c4c6be0fb920dba0359b298a93affa9d06ff3
Metin Arda Durma <metinardadurma@gmail.com>
2023-09-20 20:14:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace donglerwhile;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //While
+        Console.WriteLine("Bir sayi giriniz ");
+        int n = int.Parse(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+
+        while (sayac<= n)
+        {
+            toplam+=sayac;
+            sayac ++;
+        }
+
+        Console.WriteLine(toplam/n);
+
+        char charerdad = 'a';
+
+        while (charerdad<='z')
+        {
+            Console.WriteLine(charerdad);
+            charerdad ++;
+        } 
+
+        Console.WriteLine("***** Foreach *****");
+        string[] arabalar = {"BMV","FORD","DACİA","AUDİ","MERSEDES","FERRARİ"};
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+}
